f65a6a0f5aa2d3e32358ff0616a5900ab384c5a4
iyaanali <iyaanalimoosa@gmail.com>
2026-06-22 22:25:37 +0500

    first commit

---
+++ M4-L1-A1.py
@@ -0,0 +1,11 @@
+Empty_list = []
+print()
+numbers = [1,2,3,4,5]
+print(numbers)
+print()
+triples = [numbers*3]
+print(triples)
+print()
+Alist = [100,200,300,400,500]
+Alist = Alist [::-1]
+print(Alist)
