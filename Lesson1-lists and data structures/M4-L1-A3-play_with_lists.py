ae5e0023e20db9dd1eea0c7ec5521620eef87c8d
iyaanali <iyaanalimoosa@gmail.com>
2026-06-24 22:21:03 +0500

    first commit

---
+++ Lesson1-lists and data structures/M4-L1-A3-play_with_lists.py
@@ -0,0 +1,7 @@
+L = [4,5,1,2,9,7,10,8]
+print("Original list:", L)
+
+# variable to store the sum of
+# the list
+count = 0
+
---
+++ Lesson2-tuples/M4-L2-A1-tuple operation.py
@@ -0,0 +1,7 @@
+#Tuple operations
+tuplex1 = ("iyaan",7,False,15.2)
+tuplex2 = (1, 4, 8, 12, 6)
+print(tuplex1,tuplex2)
+tuplex3 = tuplex1 + tuplex2
+tuplex1 [2] = 8
+print(tuplex1)
