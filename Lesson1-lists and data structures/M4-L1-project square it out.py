9966a66b749eb6363bc4d25674c7e109cc6cc95c
iyaanali <iyaanalimoosa@gmail.com>
2026-07-06 21:09:31 +0500

    fist commit

--- Lesson1-lists and data structures/M4-L1-project square it out.py
+++ Lesson1-lists and data structures/M4-L1-project square it out.py
@@ -1,6 +1,13 @@
 n = int(input("Enter the size of the square: "))
 
+square = []  # List to store each row
+
 for i in range(n):
+    row = []
     for j in range(n):
-        print("*", end=" ")
-    print()
+        row.append("*")
+    square.append(row)
+
+# Print the square
+for row in square:
+    print(" ".join(row))
