df07e4f76bf169335b1cc8d974c430846f458cf0
iyaanali <iyaanalimoosa@gmail.com>
2026-07-06 21:05:34 +0500

    first commit

--- Lesson2-tuples/M4-L2-Tuples project.py
+++ Lesson2-tuples/M4-L2-Tuples project.py
@@ -1,3 +1,20 @@
-product_codes = ("P001", "P002", "P003", "P004")
+# Two tuples
+tuple1 = (2, 4, 6)
+tuple2 = (3, 5, 7)
 
-print(product_codes)
+# Calculate the product of each tuple
+product1 = 1
+product2 = 1
+
+for num in tuple1:
+    product1 *= num
+
+for num in tuple2:
+    product2 *= num
+
+# Display the results
+print("Tuple 1:", tuple1)
+print("Product of Tuple 1:", product1)
+
+print("Tuple 2:", tuple2)
+print("Product of Tuple 2:", product2)
